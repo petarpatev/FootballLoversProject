e32669e703e55c5f5fe7a0eb0af4cb31f07803df
Petar Patev <petarpatev@abv.bg>
2023-08-04 11:33:05 +0300

    Modify admin and create a footer

--- football_lovers/accounts/admin.py
+++ football_lovers/accounts/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from football_lovers.accounts.forms import EditUserForm, RegisterUserForm
+
 UserModel = get_user_model()
 
 
 @admin.register(UserModel)
 class UserModelAdmin(admin.ModelAdmin):
-    pass
+    form = EditUserForm
+    add_form = RegisterUserForm
+
+    ordering = ('id',)
+    list_display = ('id', 'username', 'first_name', 'last_name', 'gender', 'email',)
+    fieldsets = (
+        (None,
+         {
+            'fields': ('username', 'password'),
+         }),
+
+        ('Personal Information',
+         {
+             'fields': ('first_name', 'last_name', 'email', 'gender', 'profile_picture'),
+         }),
+
+        ('Permissions',
+         {
+             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+         }),
+
+        ('Date information',
+         {
+             'fields': ('last_login', 'date_joined'),
+         }),
+    )
